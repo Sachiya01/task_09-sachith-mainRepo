b0df54cf2fe78e34d7b2495698b912b78e60cafd
Sachith Liyanage <sachith.l@aeturnum.com>
2026-02-23 14:29:08 +0530

    tf files

--- Infrastructure/userdata/private_user_data.sh
+++ Infrastructure/userdata/private_user_data.sh
@@ -82,3 +82,25 @@ echo "Usermod for Jenkins completed"
 echo "Usermod for Jenkins completed"  
 echo "Usermod for Jenkins completed"
 
+
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+
+echo "Terraform installation completed"
+echo "Terraform installation completed"
+echo "Terraform installation completed"
+
+
+echo "ALL INSTALLATIONS COMPLETED"
+echo "ALL INSTALLATIONS COMPLETED"
+echo "ALL INSTALLATIONS COMPLETED"
+echo "ALL INSTALLATIONS COMPLETED"
+echo "ALL INSTALLATIONS COMPLETED"
+echo "ALL INSTALLATIONS COMPLETED"
+echo "ALL INSTALLATIONS COMPLETED"
+echo "ALL INSTALLATIONS COMPLETED"
+echo "ALL INSTALLATIONS COMPLETED"
+echo "ALL INSTALLATIONS COMPLETED"
+
+
